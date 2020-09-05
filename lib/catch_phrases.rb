006dd56cde01718a28f2645926a3f2e44b8de8d4
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-05 23:22:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,5 @@
-def mario
+def all_phrases
+  def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -6,5 +7,10 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  old_man = "It's Dangerous To Go Alone! Take This."
+  puts old_man
 end
 
